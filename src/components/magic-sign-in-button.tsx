2fb12b963f72a263becaf11385d04cfb3ad9ba27
Jake Holman <me@jakeisonline.com>
2024-10-24 20:57:36 +0100

    awful typo

--- src/components/magic-sign-in-button.tsx
+++ src/components/magic-sign-in-button.tsx
@@ -13,7 +13,7 @@ import {
 import { Button } from "@/components/ui/button"
 import { Loader2, CircleX } from "lucide-react"
 import { useFormStatus } from "react-dom"
-import { useActionState } from "punycode"
+import { useActionState } from "react"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
