d059c4f72815cba3d2e9c515cf7873965aa22b83
Jake Holman <me@jakeisonline.com>
2024-12-29 22:40:01 +0000

    restore layout for auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,36 @@
-export default function AuthLayout({
+import type { Metadata } from "next"
+import localFont from "next/font/local"
+import "../globals.css"
+
+const geistSans = localFont({
+  src: "../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+})
+const geistMono = localFont({
+  src: "../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+})
+
+export const metadata: Metadata = {
+  title: "Next.js + Auth.js Template",
+  description:
+    "Sign up and auth, super quick, with database-backed sessions, social sign in, and magic links.",
+}
+
+export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
-  return <div className="mt-20">{children}</div>
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} min-h-dvh bg-background overflow-y-scroll overflow-x-clip flex flex-col items-center`}
+      >
+        <div className="mt-20">{children}</div>
+      </body>
+    </html>
+  )
 }
