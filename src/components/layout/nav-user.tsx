e3e2d0b10e10eef69df4c54f79b94aa08bf52bfe
Jake Holman <me@jakeisonline.com>
2024-12-30 11:50:21 +0000

    Revert "feat: add alert dialog when signing out"
    Radix is causing a problem due to focus stealing, resulting in an a11y warning in the console. cf. https://github.com/unovue/radix-vue/issues/1280
    
    This reverts commit fba7141c97badd8d5bddb71c127ab43a5732f9d7.

--- src/components/layout/nav-user.tsx
+++ src/components/layout/nav-user.tsx
@@ -8,7 +8,6 @@ import {
   Settings2,
   Sparkles,
   CircleUser,
-  Loader2,
 } from "lucide-react"
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
@@ -29,35 +28,43 @@ import {
 } from "@/components/ui/sidebar"
 import { User } from "@/db/schema/users"
 import { SignOutButton } from "@/components/ui/sign-out-button"
-import {
-  AlertDialog,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
 
 export function NavUser({ user }: { user: User }) {
   const { isMobile } = useSidebar()
 
   return (
     <SidebarMenu>
-      <AlertDialog>
-        <AlertDialogContent>
-          {/* Hack to remove radix warning about lack of a title */}
-          <AlertDialogTitle className="hidden"></AlertDialogTitle>
-          <AlertDialogDescription className="flex items-center gap-2">
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-            Signing you out...
-          </AlertDialogDescription>
-        </AlertDialogContent>
-        <SidebarMenuItem>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <SidebarMenuButton
-                size="lg"
-                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-              >
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <Avatar className="h-8 w-8 rounded-lg">
+                <AvatarImage
+                  src={user.image ?? undefined}
+                  alt={user.name ?? undefined}
+                />
+                <AvatarFallback className="rounded-lg">
+                  <CircleUser className="stroke-1" />
+                </AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">{user.name}</span>
+                <span className="truncate text-xs">{user.email}</span>
+              </div>
+              <ChevronsUpDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage
                     src={user.image ?? undefined}
@@ -71,67 +78,40 @@ export function NavUser({ user }: { user: User }) {
                   <span className="truncate font-semibold">{user.name}</span>
                   <span className="truncate text-xs">{user.email}</span>
                 </div>
-                <ChevronsUpDown className="ml-auto size-4" />
-              </SidebarMenuButton>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent
-              className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-              side={isMobile ? "bottom" : "right"}
-              align="end"
-              sideOffset={4}
-            >
-              <DropdownMenuLabel className="p-0 font-normal">
-                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                  <Avatar className="h-8 w-8 rounded-lg">
-                    <AvatarImage
-                      src={user.image ?? undefined}
-                      alt={user.name ?? undefined}
-                    />
-                    <AvatarFallback className="rounded-lg">
-                      <CircleUser className="stroke-1" />
-                    </AvatarFallback>
-                  </Avatar>
-                  <div className="grid flex-1 text-left text-sm leading-tight">
-                    <span className="truncate font-semibold">{user.name}</span>
-                    <span className="truncate text-xs">{user.email}</span>
-                  </div>
-                </div>
-              </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuGroup>
-                <DropdownMenuItem>
-                  <Sparkles />
-                  Upgrade to Pro
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-              <DropdownMenuSeparator />
-              <DropdownMenuGroup>
-                <DropdownMenuItem>
-                  <Settings2 />
-                  Preferences
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <CreditCard />
-                  Billing
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Bell />
-                  Notifications
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-              <DropdownMenuSeparator />
-              <AlertDialogTrigger asChild>
-                <DropdownMenuItem>
-                  <LogOut />
-                  <SignOutButton className="hover:no-underline p-0 h-0">
-                    Sign out
-                  </SignOutButton>
-                </DropdownMenuItem>
-              </AlertDialogTrigger>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </SidebarMenuItem>
-      </AlertDialog>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <Sparkles />
+                Upgrade to Pro
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <Settings2 />
+                Preferences
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <CreditCard />
+                Billing
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Bell />
+                Notifications
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <LogOut />
+              <SignOutButton className="hover:no-underline p-0 h-0">
+                Sign out
+              </SignOutButton>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
     </SidebarMenu>
   )
 }
