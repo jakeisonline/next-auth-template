b3f207d18c3de125028a9d7df3523b56b130c5cf
Jake Holman <me@jakeisonline.com>
2024-12-31 14:53:30 +0000

    remove card from signout page

--- src/app/(auth)/signout/page.tsx
+++ src/app/(auth)/signout/page.tsx
@@ -1,20 +1,17 @@
 import { DoSignout } from "@/components/signout"
-import { Card, CardContent } from "@/components/ui/card"
 import { Loader2 } from "lucide-react"
 
 export default function SignoutPage() {
   return (
-    <Card className="mx-auto w-[24rem]">
-      <CardContent>
-        <div className="text-center pt-10">
-          <span className="text-8xl">👋</span>
-          <p className="mt-6 flex items-center justify-center gap-1.5">
-            <Loader2 className="h-4 w-4 animate-spin inline-block" />
-            Signing you out...
-          </p>
-        </div>
-      </CardContent>
+    <>
+      <div className="text-center pt-10">
+        <span className="text-8xl">👋</span>
+        <p className="mt-6 flex items-center justify-center gap-1.5">
+          <Loader2 className="h-4 w-4 animate-spin inline-block" />
+          Signing you out...
+        </p>
+      </div>
       <DoSignout />
-    </Card>
+    </>
   )
 }
