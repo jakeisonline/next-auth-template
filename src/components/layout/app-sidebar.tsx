8c838dddd84f4f54813df2cc20bd8ddc4bb2fbaf
Jake Holman <me@jakeisonline.com>
2024-12-31 21:34:41 +0000

    replace fake company name with template name

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -80,7 +80,9 @@ export function AppSidebar({
                   <Command className="size-4" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">Acme Inc</span>
+                  <span className="truncate font-semibold">
+                    next-auth-template
+                  </span>
                 </div>
               </Link>
             </SidebarMenuButton>
