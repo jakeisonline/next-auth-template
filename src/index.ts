60f6ce35424d2417d37151134c5bda80e60b88d2
Jake Holman <me@jakeisonline.com>
2025-01-02 13:16:50 +0000

    use direct import

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Command } from "commander"
 import { init } from "@/commands/init"
 
 // Get the package.json file
-const packageJson = require("../package.json")
+import packageJson from "../package.json"
 
 async function main() {
   const program = new Command()
