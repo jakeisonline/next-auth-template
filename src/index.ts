105740899e660c56710228073f88941820d6850b
Jake Holman <me@jakeisonline.com>
2025-01-03 20:53:01 +0000

    fix: be a little more graceful when exiting

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import { init } from "@/commands/init"
 // Get the package.json file
 import packageJson from "../package.json"
 
+// Handle graceful exit
+process.on("SIGINT", () => process.exit(0))
+process.on("SIGTERM", () => process.exit(0))
+
 async function main() {
   const program = new Command()
     .name("next-auth-template")
