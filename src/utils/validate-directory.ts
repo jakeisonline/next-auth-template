a478f5ab6376f02fc38cfb7f500d0bf901047ed8
Jake Holman <me@jakeisonline.com>
2025-01-03 20:51:29 +0000

    fix: check if the directory exists before checking there are files

--- src/utils/validate-directory.ts
+++ src/utils/validate-directory.ts
@@ -1,4 +1,4 @@
-import { readdirSync } from "node:fs"
+import { readdirSync, existsSync } from "node:fs"
 import { createSpinner } from "@/utils/spinner"
 import prompts from "prompts"
 
@@ -8,19 +8,21 @@ export async function validateDirectory(
   spinner: ReturnType<typeof createSpinner>,
 ) {
   try {
-    const files = await readdirSync(targetDir)
-    if (files.length > 0 && !overwrite) {
-      const { overwritePrompt } = await prompts({
-        type: "confirm",
-        name: "overwritePrompt",
-        message:
-          "Directory is not empty. Files will be overwritten and existing files may cause conflicts. Proceed?",
-        initial: false,
-      })
+    if (existsSync(targetDir)) {
+      const files = await readdirSync(targetDir)
+      if (files.length > 0 && !overwrite) {
+        const { overwritePrompt } = await prompts({
+          type: "confirm",
+          name: "overwritePrompt",
+          message:
+            "Directory is not empty. Files will be overwritten and existing files may cause conflicts. Proceed?",
+          initial: false,
+        })
 
-      if (!overwritePrompt) {
-        spinner.fail("Aborted.")
-        process.exit(1)
+        if (!overwritePrompt) {
+          spinner.fail("Aborted.")
+          process.exit(1)
+        }
       }
     }
   } catch (err) {
