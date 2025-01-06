ff8cb61646ebd90d9089e866e487d8f05c7edb79
Jake Holman <me@jakeisonline.com>
2025-01-06 23:15:35 +0000

    chore(one-to-one): remove local fonts as they are not being used (#7)

---
+++ .changeset/gentle-islands-prove.md
@@ -0,0 +1,6 @@
+---
+"next-auth-template-one-to-one": minor
+"next-auth-template": patch
+---
+
+chore(one-to-one): remove local fonts as they are not being used
--- templates/one-to-one/src/app/(site)/layout.tsx
+++ templates/one-to-one/src/app/(site)/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next"
-import localFont from "next/font/local"
 import "../globals.css"
-
-const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-})
-const geistMono = localFont({
-  src: "../fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-})
 
 export const metadata: Metadata = {
   title: "Next.js + Auth.js Template",
@@ -26,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} min-h-dvh bg-background overflow-y-scroll overflow-x-clip flex flex-col items-center`}
-      >
+      <body className="bg-background flex min-h-dvh flex-col items-center overflow-x-clip overflow-y-scroll">
         {children}
       </body>
     </html>
