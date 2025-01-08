1459deb98ff2ecc5259c96d4bedce4c898664922
Jake Holman <me@jakeisonline.com>
2025-01-08 22:26:53 +0000

    feat(one-to-many): add name field to accounts table

--- templates/one-to-many/src/db/schema/accounts.ts
+++ templates/one-to-many/src/db/schema/accounts.ts
@@ -4,6 +4,7 @@ export const accountsTable = table("accounts", {
   id: text("id")
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
+  name: text("name").notNull(),
   updatedAt: timestamp("updated_at")
     .notNull()
     .$onUpdate(() => new Date()),
