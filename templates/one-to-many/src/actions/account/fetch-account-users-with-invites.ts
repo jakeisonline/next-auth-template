458891505420f86277c48e9c40bf7b1411841d13
Jake Holman <me@jakeisonline.com>
2025-01-09 23:05:04 +0000

    perf(one-to-many): batch db calls

--- templates/one-to-many/src/actions/account/fetch-account-users-with-invites.ts
+++ templates/one-to-many/src/actions/account/fetch-account-users-with-invites.ts
@@ -5,28 +5,30 @@ import { eq, and, isNotNull } from "drizzle-orm"
 import { inviteTokensTable } from "@/db/schema/invite_tokens"
 
 export async function fetchAccountUsersWithInvites(accountId: string) {
-  const userResults = await db
-    .select({
-      id: usersTable.id,
-      name: usersTable.name,
-      email: usersTable.email,
-      image: usersTable.image,
-      status: usersAccountsTable.status,
-      role: usersAccountsTable.role,
-    })
-    .from(usersAccountsTable)
-    .leftJoin(usersTable, eq(usersAccountsTable.userId, usersTable.id))
-    .where(
-      and(
-        eq(usersAccountsTable.accountId, accountId),
-        isNotNull(usersTable.id),
+  const [userResults, inviteResults] = await db.batch([
+    db
+      .select({
+        id: usersTable.id,
+        name: usersTable.name,
+        email: usersTable.email,
+        image: usersTable.image,
+        status: usersAccountsTable.status,
+        role: usersAccountsTable.role,
+      })
+      .from(usersAccountsTable)
+      .leftJoin(usersTable, eq(usersAccountsTable.userId, usersTable.id))
+      .where(
+        and(
+          eq(usersAccountsTable.accountId, accountId),
+          isNotNull(usersTable.id),
+        ),
       ),
-    )
 
-  const inviteResults = await db
-    .select()
-    .from(inviteTokensTable)
-    .where(eq(inviteTokensTable.accountId, accountId))
+    db
+      .select()
+      .from(inviteTokensTable)
+      .where(eq(inviteTokensTable.accountId, accountId)),
+  ])
 
   return [
     ...userResults.map((user) => ({ type: "user" as const, ...user })),
