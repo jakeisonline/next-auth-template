9ccfa01d4684a1858bfee957215e834d634ce839
Jake Holman <me@jakeisonline.com>
2025-01-10 21:12:39 +0000

    fix(one-to-many): perform some integrity checks before removing a user

--- templates/one-to-many/src/actions/account/do-remove-user.ts
+++ templates/one-to-many/src/actions/account/do-remove-user.ts
@@ -6,6 +6,7 @@ import { auth } from "@/lib/auth"
 import { and, eq } from "drizzle-orm"
 import { revalidatePath } from "next/cache"
 import { z } from "zod"
+import { fetchAccountUsers } from "./fetch-account-users"
 
 export async function doRemoveUser(prevState: any, formData: FormData) {
   if (!(formData instanceof FormData)) {
@@ -29,6 +30,7 @@ export async function doRemoveUser(prevState: any, formData: FormData) {
   const userId = z.string().uuid().safeParse(formData.get("userId"))
   const accountId = z.string().uuid().safeParse(formData.get("accountId"))
 
+  // Validate the user and account ID
   if (!userId.success || !accountId.success) {
     return {
       status: "error",
@@ -36,6 +38,52 @@ export async function doRemoveUser(prevState: any, formData: FormData) {
         {
           title: "Invalid user or account ID",
           body: "The user or account ID is not a valid UUID",
+        },
+      ],
+    }
+  }
+
+  // Check the user is not the current user
+  if (session.user.id === userId.data) {
+    return {
+      status: "error",
+      messages: [
+        {
+          title: "User not removed",
+          body: "You cannot remove yourself",
+        },
+      ],
+    }
+  }
+
+  const users = await fetchAccountUsers(accountId.data, ["admin", "owner"])
+
+  // Check if the user creating an invite is an admin or owner for the account
+  if (!users.some((user) => user.users.id === session.user.id)) {
+    return {
+      status: "error",
+      messages: [
+        {
+          title: "User not removed",
+          body: "You are not an admin or owner for this account",
+        },
+      ],
+    }
+  }
+
+  // Check the user is not the owner of the account
+  if (
+    users.some(
+      (user) =>
+        user.users.id === userId.data && user.users_accounts.role === "owner",
+    )
+  ) {
+    return {
+      status: "error",
+      messages: [
+        {
+          title: "User was not removed",
+          body: "Owner of the account cannot be removed.",
         },
       ],
     }
@@ -53,7 +101,12 @@ export async function doRemoveUser(prevState: any, formData: FormData) {
   if (result.rowCount === 0) {
     return {
       status: "error",
-      messages: [{ title: "User not deleted", body: "The user was not found" }],
+      messages: [
+        {
+          title: "User not removed",
+          body: "The user you are trying to remove may have already been removed",
+        },
+      ],
     }
   }
 
