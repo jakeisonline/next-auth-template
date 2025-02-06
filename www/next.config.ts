4e71cd6fc298d5fb98bc3521fe796352cb2fa506
Jake Holman <me@jakeisonline.com>
2025-02-06 20:34:47 +0000

    set contentDirBasePath for nextra

--- www/next.config.ts
+++ www/next.config.ts
@@ -1,7 +1,7 @@
 import nextra from "nextra"
 
 const withNextra = nextra({
-  // ... Other Nextra config options
+  contentDirBasePath: "/docs",
 })
 
 // You can include other Next.js configuration options here, in addition to Nextra settings:
