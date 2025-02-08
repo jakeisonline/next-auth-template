a691b77cacc8ccd18642d0ae312e9ecf83b4ec4d
Jake Holman <me@jakeisonline.com>
2025-02-08 21:13:57 +0000

    fix incorrect bg for code

--- www/src/components/mdx/code.tsx
+++ www/src/components/mdx/code.tsx
@@ -9,7 +9,7 @@ export const Code: FC<ComponentProps<"code">> = ({
   return (
     <code
       className={cn(
-        "bg-gray-100 p-1 rounded-md border border-border font-normal",
+        "bg-accent p-1 rounded-md border border-border font-normal",
         className,
       )}
       {...props}
